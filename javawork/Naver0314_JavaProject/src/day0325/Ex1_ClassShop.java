46e454de042baadc9dedb756f1f659e9f7f51fe0
alswl11 <parkminji08@naver.com>
2024-03-25 12:42:06 +0900

    0325_수업

---
+++ javawork/Naver0314_JavaProject/src/day0325/Ex1_ClassShop.java
@@ -0,0 +1,58 @@
+package day0325;
+
+public class Ex1_ClassShop {
+	
+	// showTtile
+	public static void showTitle() {
+		System.out.println("번호\t상품\t수량\t단가\t총금액");
+		System.out.println("=".repeat(50));
+	}
+	
+	// writeShop
+	public static void writeShop(Shop [] myShop) {
+		
+		for(int i = 0; i<myShop.length; i++) {
+			System.out.println(i + "\t" + myShop[i].getSang() + "\t" + myShop[i].getSu() + "\t" + myShop[i].getDan() + "\t"+ myShop[i].getTot());
+		}
+	
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+//		Shop [] myShop = new Shop[4]; // 객체 배열 메모리 할당, 초기값은 null 
+//		myShop[0] = new Shop(); // default는 0번째로 설정 
+//		myShop[1] = new Shop("딸기");
+//		myShop[2] = new Shop(3,2000);
+//		myShop[3] = new Shop("사과", 5, 3000);
+		
+		// 배열 할당 시 생성자 호출하는 방식 
+		Shop [] myShop = {
+				new Shop(), new Shop("딸기"), 
+				new Shop(3,2000), new Shop("사과", 5, 3000)
+		};
+		
+	
+		showTitle(); // 제목 출력하는 static 메서드 < 번호 상품 수량 단가 총 금액 > 
+		writeShop(myShop);
+		
+		System.out.println();
+		System.out.println("0번의 상품명, 수량, 단가 변경");
+		myShop[0].setSang("포도");
+		myShop[0].setSu(3);
+		myShop[0].setDan(2000);
+		System.out.println();
+		
+		System.out.println("1번의 상품명, 수량, 단가 변경");
+		myShop[1].setSangpum("오렌지", 5, 1200);
+		System.out.println("\n");
+		
+		System.out.println(" ** 변경된 값으로 다시 출력 ** ");
+		showTitle();
+		writeShop(myShop);
+
+
+
+	}
+
+}
---
+++ javawork/Naver0314_JavaProject/src/day0325/Ex3_ClassEmployee.java
@@ -0,0 +1,56 @@
+package day0325;
+
+public class Ex3_ClassEmployee {
+	
+	public static void showTitle() {
+		System.out.println("번호\t이름\t직급\t나이\t기본급\t수당\t총수령액");
+		System.out.println("=".repeat(55));
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Employee [] em =  {
+				new Employee(),
+				new Employee("강호동", "사원", 30),
+				new Employee("유재석", "과장", 42),
+				new Employee("이영자", "차장", 45),
+				new Employee("손미나", 23)
+		};
+
+		// 0번 데이터 수정 
+		em[0].setName("이광수");
+		em[0].setPosition("사원");
+		em[0].setAge(24);
+		em[0].setGibon(340);
+		em[0].setSudang(23);
+
+		// 1번 데이터 수정 
+		em[1].setPay(450,100); // 기본급과 수당을 같이 수정 
+
+		// 2번 데이터 수정 
+		em[2].setSawon("유재남", "계장", 41);
+		em[2].setPay(410, 80);
+
+		// 3번 데이터 수정 
+		em[3].setPay(560, 120);
+
+		// 4번 데이터 수정 
+		em[4].setPosition("사원");
+		em[4].setPay(289,67);
+
+
+		
+		// 출력 
+		showTitle(); // static method로 제목 출력 
+
+		for(int i = 0; i<em.length; i++) {
+			System.out.println(i+1 + "\t" + em[i].getName() + "\t" + em[i].getPosition() + "\t" + em[i].getAge()
+					+ "\t" + em[i].getGibon() + "\t" + em[i].getSudang() + "\t" + em[i].getNetPay());
+		}
+
+
+
+	}
+
+}
