fafe6c949cc1a476ad3e517f4cc8fe0aa3c99b9e
alswl11 <parkminji08@naver.com>
2024-04-03 17:03:11 +0900

    0403_수업_부모자식테이블연습

--- DBwork/seq_table_prac.sql
+++ DBwork/seq_table_prac.sql
@@ -62,7 +62,7 @@ where f.foodnum = b.foodnum(+) and bname is null;
 
 -- food의 200번 메뉴는 삭제가 될까? -> ㅇㅇ, 아무도 주문한 사람이 없어서 삭제 가능
 delete from food where foodnum = 200;
--- food의 100번 메뉴는 삭제가 될까? -> ㄴㄴ, 100번 메뉴를 주문한 고객이 잇어서 삭제할 수 없음
+-- food의 100번 메뉴는 삭제가 될까? -> ㄴㄴ,  100번 메뉴를 주문한 고객이 잇어서 삭제할 수 없음
 
 -- 부모 테이블인 food를 삭제하자
 drop table food; -- error : 자식 테이블 먼저 삭제 후 부모 삭제 가능
@@ -74,6 +74,73 @@ drop table booking;
 drop table food;
 drop sequence seq_food;
 
+
+------------ new --------------
+-- 이번에는 부모 테이블의 테이터 삭제 시 서브 테이블의 데이타도 자동으로 삭제되도록 외부키 설정
+-- 시퀀스 생성
+create sequence seq_shop;
+-- shop, cart
+create table shop
+(
+    sang_no    number(3)
+        constraint shop_pk_no primary key,
+    sang_name  varchar2(30),
+    sang_price number(10)
+);
+
+select * from shop;
+
+-- 외부키 설정 시 on delete cascade을 붙이면 부모테이블 삭제 시 자식테이블에 추가된 데이터도 같이 삭제 됨
+create table cart (
+cart_no number(3) constraint cart_pk_no primary key,
+sang_no number(3),
+cnt number(3),
+cartday date,
+constraint cart_fk_no foreign key (sang_no) references shop(sang_no) on delete cascade
+);
+
+select * from cart;
+
+insert into shop values (seq_shop.nextval, '블라우스', 12000);
+insert into shop values (seq_shop.nextval, '조끼', 19000);
+insert into shop values (seq_shop.nextval, '청바자', 25000);
+insert into shop values (seq_shop.nextval, '채크코트', 99000);
+insert into shop values (seq_shop.nextval, '티셔츠', 11000);
+commit;
+
+-- cart에 원하는 상품을 담아보자
+insert into cart values (seq_shop.nextval, 2, 3,sysdate);
+insert into cart values (seq_shop.nextval, 1, 2,sysdate);
+insert into cart values (seq_shop.nextval, 5, 1,sysdate);
+commit;
+
+-- join 으로 조회
+-- cart_no, sang_no, sang_name, sang_prie, cnt, cartday(yyyy-mm-dd) 출력
+select cart_no, cnt, s.sang_no, sang_name, sang_price,
+       to_char (cartday, 'yyyy-mm-dd') cartday
+from shop s, cart c
+where s.sang_no = c.sang_no;
+
+-- 아무도 cart에 담지 않은 상품은 ?
+select cart_no, cnt, s.sang_no, sang_name, sang_price,
+       to_char (cartday, 'yyyy-mm-dd') cartday
+from shop s, cart c
+where s.sang_no = c.sang_no(+) and cart_no is null;
+
+-- on delete cascade 를 한 경우 cart에 추가된 상품도 삭제가 가능함
+-- 2번 조끼 상품을 삭제해보자
+delete from shop where sang_no = 2;
+
+-- cart를 확인해보면 cart에 잇던 2번 상품도 사라짐
+select cart_no, cnt, s.sang_no, sang_name, sang_price,
+       to_char (cartday, 'yyyy-mm-dd') cartday
+from shop s, cart c
+where s.sang_no = c.sang_no;
+
+-- 테이블, 스키마 제거 (자식 -> 부모)
+drop table cart;
+drop table shop;
+drop sequence seq_shop;
 
 
 
